d0abe671dd2944d287061cb730d697273ad6fb33
Joseph Masom <joseph.masom@listenfirstmedia.com>
2025-02-23 17:36:16 -0500

    format, document, stylize, and clean Grammar module

--- lib/grammar/grammar.ml
+++ lib/grammar/grammar.ml
@@ -1,31 +1,44 @@
 open Utils
 open Flags
 
-type 'a t =
-| Valid : ('rb, valid) Rule_bank.t * 'rb Rule_bank.idx -> [> valid ] t
-| Invalid : ('rb, any) Rule_bank.t * 'rb Rule_bank.idx -> [> invalid ] t
+type 'j t =
+  | Valid :
+      ('rb, valid) Rule_bank.t * ('rb, valid) Rule_bank.Idx.t
+      -> [> valid ] t
+  | Invalid :
+      ('rb, any) Rule_bank.t * ('rb, any) Rule_bank.Idx.t
+      -> [> invalid ] t
+
 type grammar = Types.rule
 
-let make (rb, idx) = match Rule_bank.valid rb with
-| Some vrb -> Valid (vrb, idx)
-| None -> Invalid (rb, idx)
+(* Constructor *)
+let make (rb, idx) =
+  match (Rule_bank.valid rb, Rule_bank.Idx.valid idx) with
+  | Some vrb, Some vidx -> Valid (vrb, vidx)
+  | _, _ -> Invalid (rb, idx)
 
-let valid : any t -> valid t option = function
-| Valid (vrb, idx) -> Some (Valid (vrb, idx))
-| Invalid _ -> None
+(* Analysis *)
+let valid = function
+  | Valid (vrb, idx) -> Some (Valid (vrb, idx))
+  | Invalid _ -> None
 
 let report_errs _ = assert false (* TODO: implement *)
+
+(* Compilation and usage *)
 let compile : valid t -> grammar = function
-| Valid (rb, idx) ->
-  let rb' = Rule_bank.compile rb in
-  Rule_bank.get idx rb'
+  | Valid (rb, idx) ->
+      let rb' = Rule_bank.compile rb in
+      Rule_bank.get idx rb'
 
 let gen_word (g : grammar) = g#gen_word
-
 let gen_chain (g : grammar) = g#gen_chain
 
+(* Masked sub-components *)
 module Rule_bank = struct
   include Rule_bank
 
   type 'id t = ('id, any) Rule_bank.t
+  type 'rb idx = ('rb, any) Idx.t
+
+  let idx_map_of rb = (idx_map_of rb :> 'rb idx String_map.t)
 end
--- lib/grammar/grammar.mli
+++ lib/grammar/grammar.mli
@@ -1,72 +1,140 @@
+(** Word-generating grammars.
+
+    At a minimum, a grammar consists of a context-sensitive collection of
+    word-generating rules, called a rule bank, and a pointer, or index, to one
+    of said rules.
+
+    Ape grammars are built bottom-up and keep track of the validity of their
+    sub-components as they are constructed. A grammar's validity judgement is
+    encoded in its sole type argument ['j], which is always a subtype of
+    [Utils.Flags.any]. A grammar of type [any t] is of unknown validity, whereas
+    a grammar of type [valid t] has been checked, and is ultimately able to be
+    compiled. A grammar's validity can be inspected via the [valid] function,
+    which expects an unchecked grammar and returns a [valid t option].
+
+    Ape grammars, once compiled, generate words, represented either as strings,
+    or as lists of strings indicating the series of transformations from
+    underlying to surface form. A grammar must have its validity checked before
+    it can be compiled. *)
+
 open Utils
 open Flags
 
-type 'a t
-type grammar
+type 'j t
+(** The type of grammars with validity judgement ['j]. *)
 
-(** A type-identified collection of rules. *)
+(** {1 Construction} *)
+
+(** Type-identified collections of rules. *)
 module Rule_bank : sig
   type 'id t
-  type 'rb idx
+  (** The type of rule banks uniquely identified by type ['id]. *)
 
+  type 'rb idx
+  (** The type of the indices in the rule bank ['rb]. *)
+
+  (** A modularly-encoded record containing a rule bank [value] and its
+      identifier [id]. Used as a return type for functions that construct new
+      rule banks. *)
   module type T = sig
     type id
 
     val value : id t
   end
 
-  (** A single rule, tagged with its context bank. *)
+  (** Contextualized word generation rules. *)
   module Rule : sig
     type 'rb t
+    (** The type of rules constructed in the context of rule bank ['rb]. *)
+
     type 'rb rule_bank
 
-    (** An expression for initial word generation and matching. *)
+    (** Contextualized expressions for initial word generation and matching. *)
     module Expr : sig
       type 'rb t
+      (** The type of expressions constructed in the context of rule bank ['rb].
+      *)
 
       val parse_ast : 'rb rule_bank -> Parsing.Expr.ast -> 'rb t
-      (** Constructor *)
+      (** [parse_ast rb ast] generates an expression from [ast] in the semantic
+          context of [rb]. *)
     end
 
-    (** An operation for finding and replacing substrings. *)
+    (** Contextualized operations for replacing matched substrings. *)
     module Rewrite : sig
       type 'rb t
+      (** The type of rewrite operations constructed in the context of rule bank
+          ['rb]. *)
 
-      (** A replacement operation to perform on a matched substring. *)
+      (** Contextualized replacement operations to perform on a matched
+          substring. *)
       module Repl : sig
         type 'ex t
+        (** The type of replacement operations constructed in the context of
+            expression ['ex]. *)
 
         val parse_ast : ('ex, 'rb Expr.t) Named.t -> Parsing.Repl.ast -> 'ex t
+        (** [parse_ast named_ex ast] generates a replacement from [ast] in the
+            semantic context of [Named.unpack named_ex]. *)
       end
 
       val make : ('ex, 'rb Expr.t) Named.t * 'ex Repl.t -> 'rb t
-      (** Constructor *)
+      (** [make named_ex r] constructs a rewrite that applies [r] to matching
+          occurrences of [Named.unpack named_ex]. *)
     end
 
     val make : 'rb Expr.t * 'rb Rewrite.t list * 'rb Expr.t -> 'rb t
-    (** Constructor *)
+    (** [make (expr, rws, excl)] constructs a rule that generates words by
+        sequentially applying each of [rws] to an initial word generated from
+        [expr], rejecting words matched by [excl], and that also matches strings
+        with [expr]. *)
   end
   with type 'id rule_bank := 'id t
 
-  (** Constructors *)
-
   val empty : (module T)
+  (** A packed rule bank containing no rules. *)
+
   val add : string -> 'id Rule.t -> 'id t -> (module T)
+  (** [add name r rb] returns a new (packed) rule bank that contains [r] at an
+      index denoted by [name], masking any rule previously stored with [name].
+  *)
 
-  (** Index lookup *)
+  val get_idx : 'id t -> string -> 'id idx
+  (** [get_idx rb name] returns the abstract index of the rule added with name
+      [name]. *)
 
-  val get_idx : 'id t -> string -> 'id idx option
   val idx_map_of : 'id t -> 'id idx String_map.t
+  (** [idx_map_of rb] returns a map from names to the indices in [rb]. *)
 end
 
 val make : 'rb Rule_bank.t * 'rb Rule_bank.idx -> any t
-(** Constructor *)
+(** [make rb idx] constructs a grammar whose root is the rule stored in [rb] at
+    index [idx]. *)
 
-(** Validator and handling *)
+(** {1 Analysis}*)
 
 val valid : any t -> valid t option
+(** [valid g] returns [Some vg] if [g] is valid, and otherwise returns [None].
+*)
+
 val report_errs : any t -> unit
+(** [report_errs g] reports all errors generated during the construction of [g].
+*)
+
+(** {1 Compilation and usage} *)
+
+type grammar
+(** The type of compiled grammars. *)
+
 val compile : valid t -> grammar
+(** [compile vg] returns a compiled grammar that can generate words. *)
 
 val gen_word : grammar -> string option
+(** [gen_word grammar] returns [None] if an excluding expression was matched
+    somewhere, and otherwise pseudo-randomly generates [Some word] from the root
+    rule of [grammar]. *)
+
 val gen_chain : grammar -> string Nonempty_list.t option
+(** [gen_chain grammar] returns [None] if an excluding expression was matched
+    somewhere, and otherwise pseudo-randomly generates [Some chain], where
+    [chain] represents each rewrite transformation that was applied. *)
